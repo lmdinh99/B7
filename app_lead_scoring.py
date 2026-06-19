6db976356b86a893908f727fa5f38203a7e04f2b
lmdinh99 <lmdinh1997@gmail.com>
2026-06-20 00:10:19 +0700

    Update app_lead_scoring.py

--- app_lead_scoring.py
+++ app_lead_scoring.py
@@ -10,7 +10,7 @@ from streamlit_gsheets import GSheetsConnection
 # Configuration
 conn = st.connection("gsheets", type=GSheetsConnection)
 spreadsheet_id = "https://docs.google.com/spreadsheets/d/10W14zS9dL4w1G311MRFri10_XoWCSPCNPJsmITf_--s/edit"
-df = conn.read(spreadsheet=spreadsheet_id, worksheet="Trang tính 1", ttl="10m")
+df = conn.read(spreadsheet=spreadsheet_id, worksheet="leads_for_gsheet", ttl="10m")
 
 OUTPUT_FILE = "leads_scored_report.xlsx"
 
